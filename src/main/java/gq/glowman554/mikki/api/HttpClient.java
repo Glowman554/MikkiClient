546e0633272e8268564173092a9c00eb9552cdf8
Glowman554 <glowman434@gmail.com>
2022-09-18 18:10:19 +0200

    use okhttp client

--- src/main/java/gq/glowman554/mikki/api/HttpClient.java
+++ src/main/java/gq/glowman554/mikki/api/HttpClient.java
@@ -1,12 +1,15 @@
 package gq.glowman554.mikki.api;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 import gq.glowman554.mikki.utils.Log;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class HttpClient
 {
@@ -14,28 +17,24 @@ public class HttpClient
 	{
 		Log.log("GET " + _url);
 
-		URL url = new URL(_url);
-		HttpURLConnection con = (HttpURLConnection) url.openConnection();
-		con.setRequestMethod("GET");
-		con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-		con.setRequestProperty("Accept", "application/json");
+		OkHttpClient client = new OkHttpClient();
+
+		var req = new Request.Builder();
+
+		req.url(_url);
+
+		req.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+		req.addHeader("Accept", "application/json");
+
 		for (String key : headers.keySet())
 		{
-			con.setRequestProperty(key, headers.get(key));
-		}
-		if (con.getResponseCode() != 200)
-		{
-			throw new IllegalStateException("HTTP " + con.getResponseCode());
+			req.addHeader(key, headers.get(key));
 		}
 
-		String response = "";
-		for (byte b : con.getInputStream().readAllBytes())
-		{
-			response += (char) b;
-		}
-		con.getInputStream().close();
-		con.disconnect();
-		return response;
+		Response res = client.newCall(req.build()).execute();
+
+		assert res.body() != null;
+		return res.body().string();
 
 	}
 
@@ -44,37 +43,30 @@ public class HttpClient
 		return get(_url, new HashMap<>());
 	}
 
+	@SuppressWarnings("deprecation")
 	public static String post(String _url, String body, Map<String, String> headers) throws IOException
 	{
 		Log.log("POST " + _url);
 
-		URL url = new URL(_url);
-		HttpURLConnection con = (HttpURLConnection) url.openConnection();
-		con.setDoOutput(true);
-		con.setRequestMethod("POST");
-		con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-		con.setRequestProperty("Accept", "application/json");
-		con.setRequestProperty("Content-Length", String.valueOf(body.length()));
+		OkHttpClient client = new OkHttpClient();
+
+		var req = new Request.Builder();
+
+		req.url(_url);
+		req.method("POST", RequestBody.create(MediaType.parse("text/plain"), body));
+
+		req.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+		req.addHeader("Accept", "application/json");
+
 		for (String key : headers.keySet())
 		{
-			con.setRequestProperty(key, headers.get(key));
+			req.addHeader(key, headers.get(key));
 		}
 
-		con.getOutputStream().write(body.getBytes());
+		Response res = client.newCall(req.build()).execute();
 
-		if (con.getResponseCode() != 200)
-		{
-			throw new IllegalStateException("HTTP " + con.getResponseCode());
-		}
-
-		String response = "";
-		for (byte b : con.getInputStream().readAllBytes())
-		{
-			response += (char) b;
-		}
-		con.getInputStream().close();
-		con.disconnect();
-		return response;
+		assert res.body() != null;
+		return res.body().string();
 
 	}
 
